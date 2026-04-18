baa0d6e84d4751eb25237f0de9f64a4242fdade0
Vansh Mehta <mehtavansh13042005@gmail.com>
2026-04-18 12:56:40 +0530

    Render Ping Service

--- client/src/App.js
+++ client/src/App.js
@@ -21,10 +21,24 @@ import router from './router/AppRouter';
 
 // Common Components
 import { PageLoader, GlobalSplash } from './components/common';
+import { API_BASE_URL } from './config/api';
 
 function App() {
   // Always show splash on page load / browser reload
   const [isBooting, setIsBooting] = React.useState(true);
+
+  // Setup periodic health check to keep Render free-tier backend awake
+  React.useEffect(() => {
+    // Ping every 10 minutes (600000ms)
+    // Render typically sleeps after 15 minutes of inactivity
+    const interval = setInterval(() => {
+      fetch(`${API_BASE_URL}/health-check`)
+        .then(res => res.json())
+        .catch(err => console.log('Keep-alive ping failed:', err));
+    }, 10 * 60 * 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <HelmetProvider>
